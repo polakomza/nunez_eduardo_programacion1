2c0ca788f6afebbfc7567e2aee0cafe7692b2962
Nuñez Eduardo Maximiliano <145132093+polakomza@users.noreply.github.com>
2023-09-29 23:35:25 -0300

    Add files via upload
    Ejercicios del dia 28 / 09.

---
+++ ejercicios_28_09.py
@@ -0,0 +1,71 @@
+#Ejercicio 1:
+numero1 = 27 
+print("Primer numero: ",numero1)
+
+#Ejercicio 2:
+numero2 = 54.4 
+print("Segundo numero: ",numero2)
+
+#Ejercicio 3:
+suma = numero1+numero2 
+print("Suma: ",suma)
+
+#Ejercicio 4:
+resta = numero1-numero2 
+multiplicacion = numero1*numero2
+division = numero1/numero2
+print("Resta: ",resta)
+print("Multiplicacion: ",multiplicacion)
+print("Division : ",division)
+
+#Ejercicio 5:
+nombre = "Nacho" 
+print("Nombre: ",nombre)
+
+#Ejercicio 6:
+precio = 298.50 
+print("Precio: ",precio)
+
+#Ejercicio 7:
+descuento = 0.30 
+print("Descuento del 30%")
+
+#Ejercicio 8:
+precio_final = precio*descuento 
+print("Precio final: ",precio_final)
+
+#Ejercicio 9:
+cadena = "Esto en string" 
+print("Cadena: ",cadena)
+
+#Ejercicio 10:
+longitud = len(cadena) 
+print("Longitud de cadena: ",longitud)
+
+#Ejercicio 11:
+precio = int(precio) 
+print("Precio decimal: ",precio)
+
+#Ejercicio 12:
+nombre = "Luca" 
+apellido = "Torresi"
+nombre_completo = nombre+" "+apellido
+print("Nombre completo: ",nombre_completo)
+
+#Ejercicio 13:
+edad = 20
+edad = edad-5+10
+print("Edad: ",edad)
+
+#Ejercicio 14:
+altura = 1.78
+altura = (altura*4)/3
+print("Altura: ",altura)
+
+#Ejercicio 15:
+nombre = "MAXIMILIANO NUÑEZ"
+print("Nombre en mayúsculas: ",nombre)
+print("Nombre en minusculas: ",nombre.lower())
+
+#Ejercicio 16:
+print("Nombre con primera letra en mayúsculas: ",nombre.capitalize())
