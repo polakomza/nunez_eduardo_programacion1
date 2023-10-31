9c48caf8d4a852a11040c3a4bcf8f741545a4f91
eduardomaximiliano <eduardomaximiliano12@hotmail.com>
2023-10-30 23:50:53 -0300

    practico 8 con 3 ejercicios

---
+++ practico 8.py
@@ -0,0 +1,29 @@
+import funciones
+'''1.	Escribir una función que reciba un número positivo n y devuelva la cantidad de dígitos que tiene.'''
+
+# number = 221022
+# result = funciones.count_digits(number)
+# print(f"El número {number} tiene {result} dígitos.")
+
+'''2.	Escribir una función que reciba 2 enteros n y b y devuelva True si n es potencia de b.'''
+
+# first_number = int(input("Ingrese un numero entero: "))
+# second_number = int(input("Ingrese otro numero entero para saber si el primer numero es potencia de este: "))
+
+# result = funciones.potency(first_number,second_number)
+# print("Si el numero es potencia recibira como resultado 'True', caso contrario sera 'False'.")
+# print(" ↓↓")
+# print(result)
+
+'''4.	Escribir dos funciones mutualmente recursivas par(n) e impar(n) que determinen la paridad del
+numero natural dado,conociendo solo que:
+•	1 es impar.
+•	Si un número es impar, su antecesor es par; y viceversa.
+'''
+# number = int(input("Ingrese un numero para concoer si es par o impar: "))
+
+# if funciones.even(number):
+#     print(f"{number} es par.")
+# else:
+#     print(f"{number} es impar.")
+
